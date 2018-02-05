24da835dfa015d2c5f85c08a31be8e099f0adfb6
Josh Meier <josh@jm-debian.home>
2018-02-05 03:45:19 -0800

    Fixed spacing.  Will need to restructure bumps into a new struct to hold all the info I need it to while also keeping an original record of all the stuff on hand too.

--- src/rot.c
+++ src/rot.c
@@ -6,29 +6,27 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 10:55:37 by jmeier            #+#    #+#             */
-/*   Updated: 2018/01/31 10:55:50 by jmeier           ###   ########.fr       */
+/*   Updated: 2018/02/05 02:18:28 by josh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-
-
 void    rot_suite(t_xyz *fdf)
 {
-    int r;
-    int c;
+	int r;
+	int c;
 
-    r = -1;
-    while (++r < fdf->row)
-    {
-        c = -1;
-        while (++c < fdf->col)
-            {
-                fdf->bumps[r][c] = fdf->twist[r][c];
-                rot_y(fdf, fdf->rot_y, r, c);
-                rot_z(fdf, fdf->rot_z, r, c);
-                rot_x(fdf, fdf->rot_x, r, c);
-            }
-    }
+	r = -1;
+	while (++r < fdf->row)
+	{
+		c = -1;
+		while (++c < fdf->col)
+		{
+			fdf->bumps[r][c] = fdf->twist[r][c];
+			rot_y(fdf, fdf->rot_y, r, c);
+			rot_z(fdf, fdf->rot_z, r, c);
+			rot_x(fdf, fdf->rot_x, r, c);
+		}
+	}
 }
